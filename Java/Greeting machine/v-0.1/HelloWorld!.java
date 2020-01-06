ade62a2483d47ef154fdea513a27a3f436a4e46f
Imagineee.inc <59028351+imagineeeinc@users.noreply.github.com>
2020-01-06 13:36:12 +0000

    Update HelloWorld!.java

--- Java/Greeting machine/v-0.1/HelloWorld!.java
+++ Java/Greeting machine/v-0.1/HelloWorld!.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class MyWorldClass {
+	
+	//This Was Devoloped At SwiftO Software Technologies 
+	//This Was Made On Eclipse Java IDE
+	//This Is Copyrighted 
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		Scanner MyKeyboard = new Scanner(System.in);
+	    
+		System.out.print("Hello World. Whats your name ?");
+		System.out.println("Nice to meet you "+MyKeyboard.nextLine()+". How do you feel?");
+        System.out.println("Oh nice To See you Are "+MyKeyboard.nextLine()+". What is the time?");
+        System.out.println("Oh so it is "+MyKeyboard.nextLine());
+       
+        MyKeyboard.close();
+        
+        //Cool Code
+        //What Do You Think
+	}
+
+	
+}
